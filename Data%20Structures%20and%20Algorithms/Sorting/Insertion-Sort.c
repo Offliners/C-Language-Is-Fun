c1f1c10d6ad9870738057616844b4bfa1f191c9c
Offliner <42777325+Offliners@users.noreply.github.com>
2019-08-25 12:28:29 +0800

    Create Insertion-Sort.c

---
+++ Data%20Structures%20and%20Algorithms/Sorting/Insertion-Sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void insertionSort(int array[],int size);
+void printArray(int array[],int size);
+
+int main()
+{
+	int data[] = {9,5,1,4,3};
+	int size = sizeof(data) / sizeof(data[0]);
+
+	insertionSort(data,size);
+	printf("Sorted array in ascending :\n");
+	printArray(data,size);
+}
+
+void insertionSort(int array[],int size)
+{
+	int key,j;
+
+	for(int step = 1;step < size;step++)
+	{
+		key = array[step];
+		j = step - 1;
+		for(;(key < array[j] && j >= 0);j--)
+			array[j+1] = array[j];
+		array[j+1] = key;
+	}
+}
+
+void printArray(int array[],int size)
+{
+	for(int i = 0;i < size;i++)
+		printf("%d ",array[i]);
+	printf("\n");
+}
